11b42adf2925d65bb19142b1a10682036debd002
breduarm <bryan.e.armijos@gmail.com>
2024-05-07 00:19:14 -0500

    Test actions and assertions

--- app/src/androidTest/java/com/beam/composeuitesting/presentation/MyCustomComponentTest.kt
+++ app/src/androidTest/java/com/beam/composeuitesting/presentation/MyCustomComponentTest.kt
@@ -1,10 +1,38 @@
 package com.beam.composeuitesting.presentation
 
+import androidx.compose.ui.test.assertContentDescriptionContains
+import androidx.compose.ui.test.assertContentDescriptionEquals
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsFocused
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.assertIsNotFocused
+import androidx.compose.ui.test.assertIsNotSelected
+import androidx.compose.ui.test.assertIsOff
+import androidx.compose.ui.test.assertIsOn
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.assertTextContains
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.doubleClick
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.longClick
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performImeAction
+import androidx.compose.ui.test.performScrollTo
+import androidx.compose.ui.test.performTextClearance
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.performTextReplacement
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipe
+import androidx.compose.ui.test.swipeDown
+import androidx.compose.ui.test.swipeLeft
+import androidx.compose.ui.test.swipeRight
+import androidx.compose.ui.test.swipeUp
 import org.junit.Rule
 import org.junit.Test
 
@@ -24,5 +52,39 @@ class MyCustomComponentTest {
         composeTestRule.onNodeWithContentDescription("img")
 
         composeTestRule.onAllNodesWithTag("component-1")
+
+        // Actions
+        composeTestRule.onNodeWithText(text = "hey", ignoreCase = true).performClick()
+        composeTestRule.onNodeWithText(text = "hey", ignoreCase = true).performTouchInput {
+            longClick()
+            doubleClick()
+            swipeDown()
+            swipeUp()
+            swipeLeft()
+            swipeRight()
+        }
+        composeTestRule.onNodeWithText(text = "hey", ignoreCase = true).performScrollTo().performClick()
+        composeTestRule.onNodeWithText(text = "hey", ignoreCase = true).performImeAction()
+        composeTestRule.onNodeWithText(text = "hey", ignoreCase = true).performTextClearance()
+        composeTestRule.onNodeWithText(text = "hey", ignoreCase = true).performTextInput("This")
+        composeTestRule.onNodeWithText(text = "hey", ignoreCase = true).performTextReplacement("This")
+
+        // Assertions
+        composeTestRule.onNodeWithText(text = "hey").assertExists()
+        composeTestRule.onNodeWithText(text = "hey").assertDoesNotExist()
+        composeTestRule.onNodeWithText(text = "hey").assertContentDescriptionEquals("This")
+        composeTestRule.onNodeWithText(text = "hey").assertContentDescriptionContains("hey")
+        composeTestRule.onNodeWithText(text = "hey").assertIsDisplayed()
+        composeTestRule.onNodeWithText(text = "hey").assertIsNotDisplayed()
+        composeTestRule.onNodeWithText(text = "hey").assertIsEnabled()
+        composeTestRule.onNodeWithText(text = "hey").assertIsNotEnabled()
+        composeTestRule.onNodeWithText(text = "hey").assertIsSelected()
+        composeTestRule.onNodeWithText(text = "hey").assertIsNotSelected()
+        composeTestRule.onNodeWithText(text = "hey").assertIsFocused()
+        composeTestRule.onNodeWithText(text = "hey").assertIsNotFocused()
+        composeTestRule.onNodeWithText(text = "hey").assertIsOn()
+        composeTestRule.onNodeWithText(text = "hey").assertIsOff()
+        composeTestRule.onNodeWithText(text = "hey").assertTextEquals("")
+        composeTestRule.onNodeWithText(text = "hey").assertTextContains("")
     }
 }
--- app/src/main/java/com/beam/composeuitesting/presentation/MyCustomComponent.kt
+++ app/src/main/java/com/beam/composeuitesting/presentation/MyCustomComponent.kt
@@ -4,7 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
@@ -12,9 +17,12 @@ import com.beam.composeuitesting.R
 
 @Composable
 fun MyCustomComponent() {
+    var name by remember { mutableStateOf("") }
+
     Column(modifier = Modifier.fillMaxSize()) {
+        TextField(value = name, onValueChange = { name = it })
         Text(text = "Hey", modifier = Modifier.testTag("component-1"))
-        Text(text = "Hey", modifier = Modifier.testTag("component-2"))
+        Text(text = "Your name is $name")
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
             contentDescription = "img"
